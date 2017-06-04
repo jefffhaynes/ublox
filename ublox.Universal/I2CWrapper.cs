9f5ef10d2653e18c58815d85dd4644120c5d273d
jefffhaynes <feedyurhed@gmail.com>
2017-06-04 16:00:22 -0400

    added prototype i2cwrapper

---
+++ ublox.Universal/I2CWrapper.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Windows.Devices.I2c;
+using ublox.Core;
+
+// ReSharper disable InconsistentNaming
+
+namespace ublox.Universal
+{
+    public class I2cWrapper : ISerialDevice
+    {
+        private readonly I2cDevice _i2cDevice;
+
+        public I2cWrapper(I2cDevice i2cDevice)
+        {
+            _i2cDevice = i2cDevice;
+        }
+
+        public void Write(byte[] data)
+        {
+            _i2cDevice.Write(data);
+            throw new System.NotImplementedException();
+        }
+
+        public Task<byte[]> ReadAsync(uint count, CancellationToken cancellationToken)
+        {
+            var data = new byte[count];
+            _i2cDevice.Read(data);
+            return Task.FromResult(data);
+        }
+    }
+}
